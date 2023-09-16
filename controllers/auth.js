7c10e40e231c016b2db44c36e1945817298cc0bc
AllaKaplia <kaplia.alla18@gmail.com>
2023-09-16 19:20:52 +0300

    hw-05

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const bcrypt = require('bcrypt');
 const {User} = require('../models/user');
 const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const path = require('path');
+const fs = require('fs/promises');
+const Jimp = require("jimp");
 
 const { HttpError, contrWrapper } = require('../helpers');
 
 const {SECRET_KEY} = process.env;
+
+const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
 const register = async (req, res) => {
     const {email, password} = req.body;
@@ -15,8 +21,9 @@ const register = async (req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
 
     res.status(201).json({
         'user': {
@@ -75,9 +82,29 @@ const logout = async (req, res) => {
     res.status(204).json({});
 };
 
+const updateAvatar = async (req, res) => {
+    const {_id} = req.user;
+    const {path: tempUpload, originalname} = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+
+    const avatar = await Jimp.read(tempUpload);
+    await avatar.resize(250, 250).write(resultUpload);
+
+    const avatarURL = path.join('avatars', filename);
+    await fs.rename(tempUpload, resultUpload);
+
+    await User.findByIdAndUpdate(_id, {avatarURL});
+
+    res.status(200).json({
+        avatarURL,
+    });
+};
+
 module.exports = {
     register: contrWrapper(register),
     login: contrWrapper(login),
     getCurrent: contrWrapper(getCurrent),
     logout: contrWrapper(logout),
+    updateAvatar: contrWrapper(updateAvatar),
 };
